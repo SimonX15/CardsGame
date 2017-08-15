956863388e89f5cf29a08af0c0681b35af23a059
xiaowei <SimonX15@163.com>
2017-08-15 18:21:54 +0800

    0.2：rx2

--- base/src/main/java/com/app/simon/base/BaseActivity.kt
+++ base/src/main/java/com/app/simon/base/BaseActivity.kt
@@ -1,10 +1,19 @@
 package com.app.simon.base
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 
+/**
+ * desc: BaseActivity
+ * date: 2017/8/15
+ *
+ * @author xw
+ */
 abstract class BaseActivity : AppCompatActivity() {
+
+    private var progressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,4 +30,49 @@ abstract class BaseActivity : AppCompatActivity() {
             }
         }
     }
+
+    /**
+     * show a progress dialog with the given message.
+     */
+    fun showProgressDialog(msg: String) {
+        if (progressDialog == null) {
+            progressDialog = ProgressDialog(this)
+        }
+        progressDialog!!.run {
+            setMessage(msg)
+            show()
+        }
+    }
+
+    /**
+     * show a progress dialog with the given message.
+     */
+    fun showProgressDialog(msg: String, cancelable: Boolean) {
+        if (null == progressDialog) {
+            progressDialog = ProgressDialog(this)
+            progressDialog!!.run {
+                setCancelable(cancelable)
+                setCanceledOnTouchOutside(false)
+            }
+        }
+
+        if (!progressDialog!!.isShowing) {
+            if (!isFinishing) {
+                progressDialog!!.run {
+                    show()
+                    setMessage(msg)
+                }
+            }
+        }
+    }
+
+    /**
+     * close a progress if there is one.
+     */
+    fun dismissProgressDialog() {
+        if (progressDialog != null && progressDialog!!.isShowing) {
+            progressDialog!!.dismiss()
+        }
+    }
+
 }
